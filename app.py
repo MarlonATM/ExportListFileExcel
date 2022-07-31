5d60a5bda50e668bf89c619a5499c9e7938e4212
MarlonATM <105558096+MarlonATM@users.noreply.github.com>
2022-07-31 15:13:27 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from tkinter import *
 import os
 import pandas as pd
 
+#tkinter definitions
 root = Tk()
-root.title('VN2R App')
-root.iconbitmap('Y:/00-MARLON MATOS/icone.ico')
-root.geometry('600x200')
+root.title('VN2R App') #Title of application. Showed in the header.
+root.iconbitmap('Y:/00-MARLON MATOS/icone.ico') #Icon of the application. Showed in the header.
+root.geometry('600x200') #Height and width of the window
 
-# list to store the path and name of files
-res = []
+res = [] #Defining a variable to be a list
+
+#function that runs when clicked in the button
 def code():
     for root, dirs, files in os.walk(txtDiretorio.get()):
         for file in files:
@@ -21,18 +23,23 @@ def code():
     df.to_excel(writer, sheet_name='sheet1', index=False)
     writer.save()
 
+#First text that appears in the app
 diretorio = Label(root, text='Especifique o diretório raiz:')
 diretorio.pack()
 
+#First textbox that appears in the app. Input for the path of folder.
 txtDiretorio = Entry(root, width=80)
 txtDiretorio.pack()
 
+#Second text that appears in the app
 ext = Label(root, text='Especifique a extensão pretendida (Ex: .dwg):')
 ext.pack()
 
+#Second textbox that appears in the app. Input for the path of folder.
 txtExt = Entry(root, width=20)
 txtExt.pack()
 
+#Create button
 enviar = Button(root, text='Enviar', command=code)
 enviar.pack()
 
